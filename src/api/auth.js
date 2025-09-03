f9860f93f38ccdfd672d3abbe381b4dc19adbcac
Amir Haroun <amirharounofficial@gmail.com>
2025-09-03 20:33:49 +0300

    Update API base URL to use the correct endpoint for authentication and tasks

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://eluxesa.com/api'
+const API_BASE_URL = 'https://todo.eylx.sa/api'
 
 export const authAPI = {
   login: async (email, password) => {
