f510a0d4cb0575daf3768b30f74b6cc7cd625163
Amir Haroun <amirharounofficial@gmail.com>
2025-09-03 21:50:50 +0300

    Fix API base URL to use the correct endpoint for authentication and tasks

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://todo.eylx.sa/public/api'
+const API_BASE_URL = 'https://todo.eylx.sa/api'
 
 export const authAPI = {
   login: async (email, password) => {
